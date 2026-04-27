2b54ca596781969d4d480a3ebc3aa498070d5edf
Maks Michalski <mamich1998@gmail.com>
2026-04-27 08:18:16 +0200

    Project Init

---
+++ App/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Runtime.InteropServices;
+
+namespace App;
+
+internal class Program
+{
+    [DllImport("user32.dll")]
+    public static extern bool SetCursorPos(int X, int Y);
+
+    public static async Task Waiter()
+    {
+        var minuteInSec = 60;
+        var secondInMilisec = 1000;
+        for (var i = 0; i < minuteInSec; i++)
+        {
+            Console.Write($"\rPosition will be changed from: {minuteInSec - i}");
+            await Task.Delay(secondInMilisec);
+        }
+    }
+
+    private static async Task Main(string[] args)
+    {
+        var maxHeight = Console.LargestWindowHeight;
+        var maxWidth = Console.LargestWindowWidth;
+        var random = new Random();
+
+        Console.WriteLine($"New cursor position    X:{0}    Y:{0}");
+        SetCursorPos(0, 0);
+
+        while (true)
+        {
+            var height = random.Next(0, maxHeight);
+            var width = random.Next(0, maxWidth);
+
+            await Waiter();
+
+            Console.Clear();
+            Console.WriteLine($"New cursor position    X:{width}    Y:{height}");
+            SetCursorPos(width, height);
+        }
+    }
+}
